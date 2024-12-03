b518b92fda0cdea32549710c7ac24e62895b9435
tcaouette <tobias.caouette@gmail.com>
2024-12-03 09:33:34 -0800

    remove

--- data_quality_suite/core/abstract_profile.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from .config import DataProfileConfig, DataSourceConfig
-
-class AbstractProfile(ABC):
-    @abstractmethod
-    async def create_profile(self, data, source: DataSourceConfig) -> DataProfileConfig:
-        pass
--- data_quality_suite/core/abstract_report.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from .config import QualityReportConfig
-
-class AbstractReport(ABC):
-    @abstractmethod
-    async def generate(self, report: QualityReportConfig) -> str:
-        pass
